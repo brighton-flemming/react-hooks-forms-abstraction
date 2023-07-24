d8b75ee58ebe8ea71ec548ec1c042a967e2b0985
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-24 14:49:46 +0300

    Mission Passed

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
 
-function Form({ formData, setFormData}) {
+function Form({ formData, setFormData }) {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
+
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  }
 
   function handleFirstNameChange(event) {
     setFormData({
@@ -20,8 +34,24 @@ function Form({ formData, setFormData}) {
 
   return (
     <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
+      <input
+        type="text"
+        name="firstName"
+        onChange={handleFirstNameChange}
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        name="lastName"
+        onChange={handleLastNameChange}
+        value={formData.lastName}
+      />
+      <input
+        type="checkbox"
+        name="admin"
+        onChange={handleChange}
+        checked={formData.admin}
+      />
       <button type="submit">Submit</button>
     </form>
   );
